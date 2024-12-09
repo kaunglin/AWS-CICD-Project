a8452ff5c702b0ccb207aa6da4446e3a7926d044
Kaung Lin <kaung.lin@justaftermidnight.io>
2024-12-09 16:17:40 +0800

    Added one docker step to remove the existing containers

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,15 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull sreedhar8897/hotel-app:latest
 
+echo "Checking for existing container on port 80..."
+existing_container=$(docker ps --filter "ancestor=sreedhar8897/hotel-app:latest" -q)
+
+if [ -n "$existing_container" ]; then
+  echo "Stopping existing container..."
+  docker stop $existing_container
+  echo "Removing existing container..."
+  docker rm $existing_container
+fi
+
 # Run the Docker image as a container
 docker run -dit -p 80:80 sreedhar8897/hotel-app
